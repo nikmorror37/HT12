9c868ca9b75da644ff43e43ea92221d469acf23d
nikmorror37 <nikitamironchuk37@gmail.com>
2023-03-13 00:14:06 +0300

    commit1

---
+++ HT13Serialize/HT13Serialize/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HT13Serialize
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
